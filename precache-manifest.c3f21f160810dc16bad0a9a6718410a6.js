6a4da31768b28ef20179668c0e2a8be4a41d97e0
deepak kumar <deepak.kumar13@oyorooms.com>
2020-10-15 11:55:26 +0530

    Updates

---
+++ precache-manifest.c3f21f160810dc16bad0a9a6718410a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facc8af5d604b364a7c6cafa4a2e4254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a7aa18e9d77e21d5df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6b034a6d7ee46adf51a",
+    "url": "/static/js/2.2e98d5bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2e98d5bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a7aa18e9d77e21d5df",
+    "url": "/static/js/main.8a1c01c5.chunk.js"
+  },
+  {
+    "revision": "f9d445b1790dce48dcd5",
+    "url": "/static/js/runtime-main.904b5f80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
